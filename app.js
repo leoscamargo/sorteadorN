52c9f34f4df2696593e43150e7679fddb70393d5
Leonardo <leogne98@hotmail.com>
2024-06-09 15:58:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumAleatorio(de, ate);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
+}
+function obterNumAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container_botao-desabilitado')){
+        botao.classList.remove('container_botao-desabilitado');
+        botao.classList.add('container_botao');
+    }else {
+        botao.classList.remove('container_botao');
+        botao.classList.add('container_botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = 'Números sorteados: nenhum até agora!'
+
+    alterarStatusBotao();
+}
